b6f6334da6f7b45cd251a4a856c522b1589487f6
vokhrimenko <vokhrimenko@brightgrove.com>
2020-03-29 17:01:17 +0200

    SP-2 Add UI part   - add ui: template and js   - restructured

---
+++ chat/static/chat.js
@@ -0,0 +1,27 @@
+const connection = new WebSocket('ws://localhost:8080');
+
+connection.onopen = () => {
+  console.log('connected');
+};
+
+connection.onclose = () => {
+  console.error('disconnected');
+};
+
+connection.onerror = (error) => {
+  console.error('failed to connect', error);
+};
+
+connection.onmessage = (event) => {
+  console.log('received', event.data);
+  let li = document.createElement('li');
+  li.innerText = event.data;
+  document.querySelector('#chat').append(li);
+};
+
+document.querySelector('form').addEventListener('submit', (event) => {
+  event.preventDefault();
+  let message = document.querySelector('#message').value;
+  connection.send(message);
+  document.querySelector('#message').value = '';
+});
